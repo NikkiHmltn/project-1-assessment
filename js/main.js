05b0639aab98687b45e5f1af7eedc1a7396875d1
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-11-12 07:56:11 -0800

    [js css html] add files, create functions to add and subtract the values from the display, add style and function to make it red if negative

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let infoArea = document.getElementById('info')
+let inputForm = document.querySelector('#number')
+let minus = document.querySelector('#minus')
+let plus = document.querySelector('#plus')
+
+plus.addEventListener('click', function(){
+    let result = parseInt(infoArea.textContent) + parseInt(inputForm.value)
+    document.getElementById('info').textContent = result
+})
+
+minus.addEventListener('click', function(){
+    let result = parseInt(infoArea.textContent) - parseInt(inputForm.value)
+    document.getElementById('info').textContent = result
+
+    if (result < 0) {
+        infoArea.style.color = "red";
+    }
+})
